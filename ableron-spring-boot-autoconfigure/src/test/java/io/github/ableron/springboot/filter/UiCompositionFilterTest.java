6d1fd092de66955063817859efdb2e352427a8ae
ableron <tbcollect.ableron@web.de>
2023-07-06 11:25:14 +0200

    fix toMap failing with duplicate key (#37)

--- ableron-spring-boot-autoconfigure/src/test/java/io/github/ableron/springboot/filter/UiCompositionFilterTest.java
+++ ableron-spring-boot-autoconfigure/src/test/java/io/github/ableron/springboot/filter/UiCompositionFilterTest.java
@@ -20,7 +20,6 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 
